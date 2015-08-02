05a851c3560a001542971a291c6a47d28090771c
Emilio Markgraf <emilio.markgraf@krankikom.de>
2015-08-02 19:01:54 +0200

    Adds provider cache file which lists all generated providers.

--- src/ProviderFassadeBehavior/ProviderFassadeBehavior.php
+++ src/ProviderFassadeBehavior/ProviderFassadeBehavior.php
@@ -10,4 +10,22 @@ require_once 'ProviderFassadeBehaviorBuilder.php';
 class ProviderFassadeBehavior extends Behavior
 {
     protected $additionalBuilders = array('ProviderFassadeBehaviorBuilder');
+
+    public function __construct()
+    {
+        if (is_callable('parent::__construct')) {
+            call_user_func_array(parent::__construct, func_get_args());
+        }
+
+        // get cache dir and clear cache
+        if (!defined('BEHAVIOR_PROVIDER_FASSADE_CACHE_FILE')) {
+            $r = new ReflectionObject($this);
+            $dirname = dirname($r->getFileName());
+            $cacheFile = $dirname.'/../../cache/providerCache.json';
+            define('BEHAVIOR_PROVIDER_FASSADE_CACHE_FILE', $cacheFile);
+        }
+
+        unlink(BEHAVIOR_PROVIDER_FASSADE_CACHE_FILE);
+        touch(BEHAVIOR_PROVIDER_FASSADE_CACHE_FILE);
+    }
 }
